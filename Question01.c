2ecdd27875e0d4df0a5395ab1b852ddb6bb7975c
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-02-13 16:10:44 +0530

    Question 01

---
+++ Question01.c
@@ -0,0 +1,21 @@
+// Function Prototype
+void displayUser(void);
+
+// Main Function
+#include <stdio.h>
+
+int main() {
+    displayUser();
+    return 0;
+}
+
+// Display User Function
+void displayUser(void) {
+    printf("\n============================================================\n");
+    printf("                      User Information           \n");
+    printf("============================================================\n");
+    printf("Name          : A.W.W.A. Nipun Lakshan\n");
+    printf("Email Address : awwanipunlakshan@gmail.com\n");
+    printf("LinkedIn Link : https://www.linkedin.com/in/william-a-nipun/\n");
+    printf("============================================================\n\n");
+}
