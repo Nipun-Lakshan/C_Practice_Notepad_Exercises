88011d64c0138bae55934acbae5f8561c633c0fa
Nipun Lakshan <nipunlakshan81@gmail.com>
2025-02-19 17:41:17 +0530

    Question 05

---
+++ Question05.c
@@ -0,0 +1,59 @@
+// Importing printf() and scanf() Functions From C Standard Library
+# include <stdio.h>
+
+// Prototype Function
+void printList(long long list[], long long n);
+
+// Main Function
+int main(){
+	
+	// Header String Formatting
+	printf("\n====================");
+	printf("\nComma Seperated List");
+	printf("\n====================\n\n");
+	
+	// Variable Declaration
+	long long n; // Array Size
+	long long i; // Loop Index
+	
+	// Request the Size of List of Numbers From The User
+	printf("Enter the size of the list : ");
+	scanf("%lld", &n);
+	printf("\n");
+	
+	// Creating an Array From User Defined Limit
+	long long list[n];
+	
+	// Request Data From The User
+	for(i = 0; i <n; i++){
+		if(i <9){
+		printf("Enter Number 0%lld : ",(i+1));
+		scanf("%lld", &list[i]);
+		}
+		else{
+			printf("Enter Number %lld : ",(i+1));
+			scanf("%lld", &list[i]);
+		}
+	}
+	printf("\n");
+	
+	// Calling Print Elements as Comma Seperated Function
+	printList(list, n);
+	printf("\n");
+	
+	return 0;
+}
+
+// 
+void printList(long long list[], long long n){
+	printf("[");
+	for (long long i = 0; i < n; i++){
+		printf("%lld",list[i]);
+		if(i!=(n-1)){
+			printf(", ");
+		}
+		else{
+			printf("]");
+		}
+	}
+}
